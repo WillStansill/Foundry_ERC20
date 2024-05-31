33f81761bb150468918d1206f30dc7979f6ed404
Stanman <willstansill@gmail.com>
2024-05-31 14:18:21 -0400

    Initial commit

---
+++ script/Interactions.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    
+
+
+
+    function run() external {
+        address mostRecenlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        mintNftOnContract(mostRecenlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
+
+     function mintMoodNft(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
